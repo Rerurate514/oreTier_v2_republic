e054d119a1f0a5988405b459a3388b5ca0547af1
Rerurate514 <rerurateyuto@gmail.com>
2025-04-28 21:11:08 +0900

    assessmentエンティティクラスの作成

---
+++ lib/features/create_ranking/entities/assessment.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'assessment.freezed.dart';
+part 'assessment.g.dart';
+
+@freezed
+abstract class Assessment with _$Assessment {
+  const factory Assessment({
+    required String id, 
+    required String title,
+    required double star,
+    required String impressions,
+    required String imageBytes,
+    required List<String> tags
+  }) = _Assessment;
+
+  factory Assessment.fromJson(Map<String, dynamic> json) => _$AssessmentFromJson(json);
+}
