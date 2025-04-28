7ed268e7b4e5664c3e1bdc32671b4da3b40f1083
Rerurate514 <rerurateyuto@gmail.com>
2025-04-29 01:50:25 +0900

    titleコンポーネントの作成

---
+++ lib/core/components/title.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TitleText extends StatelessWidget {
+  final String title;
+  const TitleText({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: const TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 24
+      ),
+    );
+  }
+}
