58cb9cce9b26c562ee295e0e5bc16c240a2cdf65
Rerurate514 <rerurateyuto@gmail.com>
2025-05-03 19:01:20 +0900

    showSuccessDialog関数を作成

---
+++ lib/core/utils/show_success_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../components/standard_padding.dart';
+
+void showSuccessDialog(BuildContext context, String text){
+  // ignore: inference_failure_on_function_invocation
+  showDialog(
+    context: context, 
+    builder: (BuildContext context) {
+      return SimpleDialog(
+        title: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 25),
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.check,
+                  size: 75,
+                  color: Colors.green,
+                ),
+                const StandardPadding(),
+                FittedBox(
+                  child: Text(text),
+                )
+              ],
+            )
+          ),
+        ),
+        elevation: 8,
+      );
+    }
+  );
+}
